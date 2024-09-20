10a8086df4256d779427f3bad251646555984b0b
Koying <koyingchen0523@gmail.com>
2024-09-20 23:08:59 +0800

    RCA done

--- CY-lab2/Ripple_Carry_Adder_t.v
+++ CY-lab2/Ripple_Carry_Adder_t.v
@@ -1,9 +1,41 @@
 `timescale 1ns/1ps
 
-module Ripple_Carry_Adder(a, b, cin, cout, sum);
-input [8-1:0] a, b;
-input cin;
-output cout;
-output [8-1:0] sum;
+module Ripple_Carry_Adder_t;
+
+    reg [8-1:0] a = 0, b = 0;
+    wire [8-1:0] sum;
+    wire cout;
+    reg cin = 0;
+
+    Ripple_Carry_Adder RCA (
+        .a(a),
+        .b(b),
+        .cin(cin),
+        .cout(cout),
+        .sum(sum)
+    );
+
+    initial begin
+        repeat(10) begin
+            #1;
+            if (a + b + cin != sum) begin
+                $display("Error: %d + %d + %d != %d", a, b, cin, sum);
+            end
+            a = a + 23;
+            b = b + 17;
+        end
+        $finish;
+    end
+
+    // initial begin
+    //     for (b = 0; b < 256; b = b + 25) begin
+    //         #1;
+    //         if ((a + b + cin) != sum) begin
+    //             $display("Error: %d + %d + %d = %d != %d", a, b, cin, a + b + cin, sum);
+    //         end
+    //         cin = ^cin;
+    //     end
+    //     $finish;
+    // end
     
 endmodule
