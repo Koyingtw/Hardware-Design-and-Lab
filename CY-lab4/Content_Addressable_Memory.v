7e4df410e0dc6d4b6d49548b72777f0131137855
YeZ <yezxxxx405@gmail.com>
2024-10-22 16:26:35 +0800

    fix lab4 advanced Q1

--- CY-lab4/Content_Addressable_Memory.v
+++ CY-lab4/Content_Addressable_Memory.v
@@ -9,38 +9,30 @@ output reg [3:0] dout;
 output reg hit;
 
 reg [7:0] mem[15:0];
-reg started = 0;
-reg [15:0] enable;
 
 assign next_hit = (h != 16'd0);
 wire [15:0] h;
-Comparator_Array CA0(din, enable[0], mem[0], h[0]);
-Comparator_Array CA1(din, enable[1], mem[1], h[1]);
-Comparator_Array CA2(din, enable[2], mem[2], h[2]);
-Comparator_Array CA3(din, enable[3], mem[3], h[3]);
-Comparator_Array CA4(din, enable[4], mem[4], h[4]);
-Comparator_Array CA5(din, enable[5], mem[5], h[5]);
-Comparator_Array CA6(din, enable[6], mem[6], h[6]);
-Comparator_Array CA7(din, enable[7], mem[7], h[7]);
-Comparator_Array CA8(din, enable[8], mem[8], h[8]);
-Comparator_Array CA9(din, enable[9], mem[9], h[9]);
-Comparator_Array CA10(din, enable[10], mem[10], h[10]);
-Comparator_Array CA11(din, enable[11], mem[11], h[11]);
-Comparator_Array CA12(din, enable[12], mem[12], h[12]);
-Comparator_Array CA13(din, enable[13], mem[13], h[13]);
-Comparator_Array CA14(din, enable[14], mem[14], h[14]);
-Comparator_Array CA15(din, enable[15], mem[15], h[15]);
+Comparator_Array CA0(din, mem[0], h[0]);
+Comparator_Array CA1(din, mem[1], h[1]);
+Comparator_Array CA2(din, mem[2], h[2]);
+Comparator_Array CA3(din, mem[3], h[3]);
+Comparator_Array CA4(din, mem[4], h[4]);
+Comparator_Array CA5(din, mem[5], h[5]);
+Comparator_Array CA6(din, mem[6], h[6]);
+Comparator_Array CA7(din, mem[7], h[7]);
+Comparator_Array CA8(din, mem[8], h[8]);
+Comparator_Array CA9(din, mem[9], h[9]);
+Comparator_Array CA10(din, mem[10], h[10]);
+Comparator_Array CA11(din, mem[11], h[11]);
+Comparator_Array CA12(din, mem[12], h[12]);
+Comparator_Array CA13(din, mem[13], h[13]);
+Comparator_Array CA14(din, mem[14], h[14]);
+Comparator_Array CA15(din, mem[15], h[15]);
 
 wire [3:0] out;
 Priority_Encoder PE(h, out);
 
 always @(posedge clk) begin
-    if(!started) begin
-        started <= 1;
-        enable <= 16'd0;
-        dout <= 4'd0;
-        hit <= 1'd0;
-    end
     if(ren) begin
         if(next_hit) begin
             dout <= out;
@@ -54,7 +46,6 @@ always @(posedge clk) begin
     else begin
         if(!ren && wen) begin
             mem[addr] <= din;
-            enable[addr] <= 1'd1;
         end
         dout <= 4'd0;
         hit <= 1'd0;
@@ -63,12 +54,11 @@ end
 
 endmodule
 
-module Comparator_Array(din, enable, mem, hit);
+module Comparator_Array(din, mem, hit);
 input [7:0] din, mem;
-input enable;
 output hit;
 
-assign hit = (din == mem) && enable;
+assign hit = (din === mem);
 
 endmodule
 
